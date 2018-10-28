c07ee3ab7e9b7acce4e66fe219634c62c8c8ac07
Rahul Prabhudesai <techfourk@gmail.com>
2018-10-28 18:28:50 +1100

    Made Instructions.java

---
+++ jarnite/instructions.java
@@ -0,0 +1,47 @@
+//Define package
+package jarnite;
+
+//Import dependencies
+import java.util.Scanner;
+
+public class instructions {
+
+  public static void main(String[] args) {
+    GameInstructions();
+  }
+
+// ----------[ INSTRUCTIONS ]----------
+
+  static void GameInstructions() {
+    //List of Commands Output
+    System.out.println("");
+    System.out.println(" *******************************************************************************");
+    System.out.println(" *                   ---------[ GAME INSTRUCTIONS ]---------                   *");
+    System.out.println(" *                                                                             *");
+    System.out.println(" *   --[ OBJECTIVE ]--                                                         *");
+    System.out.println(" *  The objective of the game is to be the last one standing. Players must     *");
+    System.out.println(" *  conquer new lands, collect various items scattered accross the vast lands  *");
+    System.out.println(" *  of Jarnite and fight those in their way!                                   *");
+    System.out.println(" *                                                                             *");
+    System.out.println(" *   --[ MOVEMENT ]--                                                          *");
+    System.out.println(" *  To move throughout the map, you can chose to sneak, walk or sprint in any  *");
+    System.out.println(" *  direction where there is land. Additionally, you can drive, however, you   *");
+    System.out.println(" *  need the car keys to start the car. You don't have the tools to start it   *");
+    System.out.println(" *  without keys.                                                              *");
+    System.out.println(" *                                                                             *");
+    System.out.println(" *   --[ COMBAT ]--                                                            *");
+    System.out.println(" *  Players must fight until their death. If players do not have any weapons,  *");
+    System.out.println(" *  they must engage in a fist fight with the enemy. The enemy may have their  *");
+    System.out.println(" *  weapons stashed away, so be careful. They can throw a punch and then all   *");
+    System.out.println(" *  of a sudden, fire a round straight through your head!                      *");
+    System.out.println(" *                                                                             *");
+    System.out.println(" *   --[ HEALTH ]--                                                            *");
+    System.out.println(" *  Health packs are placed all throughout the map. They can be consumed to    *");
+    System.out.println(" *  add 25 health points. Use it wisely. Using it when you're at 99 HP will    *");
+    System.out.println(" *  give you a significant disadvantage over using it when low in health.      *");
+    System.out.println(" *                                                                             *");
+    System.out.println(" *******************************************************************************");
+    System.out.println("");
+  }
+
+}
--- jarnite/main_menu.java
+++ jarnite/main_menu.java
@@ -5,19 +5,19 @@ package jarnite;
 import java.util.Scanner;
 
 public class main_menu {
-
+  
 // ----------[ MAIN MENU ]----------
-
+  
   public static void main(String[] args) throws InterruptedException {
-
+    
     //Return to menu if invalid
     boolean invalid = false;
     while (true) {
       clear.c();
-
+      
       //Creates a new scanner called MenuInput
       Scanner menu_input = new Scanner(System.in);
-
+      
       //Prints out main menu pseudo-gui
       System.out.println(" *******************************************************************************");
       System.out.println(" *                                                                             *");
@@ -39,38 +39,36 @@ public class main_menu {
       System.out.println(" *  \u00a9 2018 | Tech4K Studios                                                    *");
       System.out.println(" *******************************************************************************");
       //No more copyright symbol problems!
-
+      
       if (invalid) {
         System.out.println(" Unknown option, try again by choosing a number!");
       }
-
+      
       //Requests input from user
       String menu_selection = menu_input.next();
       clear.c();
-
+      
       //Processes the request
       if (menu_selection.equals("1")) {
-
+        
         //Close scanner
         menu_input.close();
-
+        
         //Send user to starting location
         game.main(args);
       }
       else if (menu_selection.equals("2")) {
-
-        //TODO: add instructions
-        System.out.println(" TODO: add instructions");
-
+        commands.ListOfCommands();
+        
         //Exit to main menu prompt
         System.out.println(" Type 'back' to exit to the Main Menu.");
-
+        
         //Process user input
         while (true) {
-
+          
           //Scanner checking for user input
           String exit = menu_input.next();
-
+          
           //Check Entered Number
           if (exit.equals("back")) {
             main(args);
@@ -82,17 +80,17 @@ public class main_menu {
         }
       }
       else if (menu_selection.equals("3")) {
-        commands.ListOfCommands();
-
+        instructions.GameInstructions();
+        
         //Exit to main menu prompt
         System.out.println(" Type 'back' to exit to the Main Menu.");
-
+        
         //Process user input
         while (true) {
-
+          
           //Scanner checking for user input
           String exit = menu_input.next();
-
+          
           //Check Entered Number
           if (exit.equals("back")) {
             main(args);
@@ -104,7 +102,7 @@ public class main_menu {
         }
       }
       else if (menu_selection.equals("4")) {
-
+        
         //Terminate the program
         System.exit(0);
       }
